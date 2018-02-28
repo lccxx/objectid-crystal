5dc18431f3899fd5c639398032ba755ff453aac6
Liu Chong <liuchong@mail.com>
2018-02-28 14:54:24 +0800

    Update macaddr

--- src/objectid-crystal.cr
+++ src/objectid-crystal.cr
@@ -1,13 +1,23 @@
 require "big"
 
 class ObjectId
-  DEV_DIR = "/sys/class/net"
-  MAC_ADDR = File.open(File.join(DEV_DIR, Dir.open(DEV_DIR).children.first, "address")).read_line.strip.gsub(":", "")
+  MID = macaddr[6,6]
   PID = (Process.pid | 0x1000).to_s(16)
   COUNTER = Atomic.new Random.new.next_int.abs
 
   def self.new(timestamp=Time.now.epoch, base=36)
     counter = (COUNTER.add(1).abs | 0x10000000).to_s(16)[2,6]
-    BigInt.new("#{timestamp.to_s(16)}#{MAC_ADDR[6,6]}#{PID}#{counter}", 16).to_s base
+    BigInt.new("#{timestamp.to_s(16)}#{MID}#{PID}#{counter}", 16).to_s base
+  end
+
+  def self.macaddr
+    dir = "/sys/class/net"
+    if File.exists?(dir)
+      devs = Dir.open(dir).children
+      if devs.size > 0
+        return File.open(File.join(dir, devs.first, "address")).read_line.strip.gsub(":", "")
+      end
+    end
+    12.times.map { (rand * 16).to_i8.to_s(16) }.join
   end
 end
