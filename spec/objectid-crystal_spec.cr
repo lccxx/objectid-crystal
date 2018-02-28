acf4749177822c25fc17abded643af19b3f2976b
Liu Chong <liuchong@mail.com>
2018-02-28 15:31:56 +0800

    Update test

--- spec/objectid-crystal_spec.cr
+++ spec/objectid-crystal_spec.cr
@@ -3,11 +3,14 @@ require "./spec_helper"
 describe ObjectId do
   it "works" do
     ObjectId.new.size.should eq 19
+
     ObjectId.new(16).size.should eq 24
 
     timestamp = Time.new(2016, 2, 15, 10, 20, 30).epoch
     id = ObjectId.new(16, timestamp)
     id[0,8].to_i(16).should eq timestamp
+
+    ObjectId.new.should be < ObjectId.new
   end
 
   it "check" do
@@ -19,8 +22,8 @@ describe ObjectId do
       id16 = BigInt.new(id, 36).to_s(16)
       id16.size.should eq 24
       id16[0,8].to_i(16).should eq timestamp
-      ids << id
       id.should be > ids[i - 1] if i > 0
+      ids << id
     }
     ids.uniq.size.should eq 99999
   end
